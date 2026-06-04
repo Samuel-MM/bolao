80bd818da7053756b4080de084590ee42b7e71d0
Samuel-MM <samuelmangia@gmail.com>
2026-06-04 15:01:00 -0300

    fix: invite

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -7,6 +7,13 @@ class PoolsController < ApplicationController
 
   def invite
     @pool = Pool.find_by!(invite_token: params[:invite_token])
+
+    unless user_signed_in?
+      store_location_for(:user, request.fullpath)
+      session[:invite_pool_name] = @pool.name
+      redirect_to new_user_registration_path and return
+    end
+
     @existing_membership = @pool.pool_memberships.find_by(user: current_user)
   end
 end
